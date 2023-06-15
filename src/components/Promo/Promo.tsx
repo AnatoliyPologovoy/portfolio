7ed062bb2e8fcf56da745bae5bdaef71ba615786
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-06-15 07:14:44 +0300

    rewrite styles for promo

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import cl from './promo.module.css'
+import cl from './promo.module.scss'
 import photo from '../../assets/myPhoto.jpg'
-import {Title} from "../common/Title/Title";
 
 const Promo = () => {
     return (
         <section className={cl.promo}>
             <div className={cl.promoWrapper}>
-                <Title>
-                    Привет!
-                    Меня зовут Анатолий Пологовой.
-                    Я front-end разработчик.
-                </Title>
+                <div className={cl.title}>
+                    Hello I'm
+                    <h1 className={cl.name}> Anatoliy Pologovoy</h1>
+                    Front-end developer
+                </div>
                 <img src={photo}
                      alt="Портрет молодого мужчины, светлая кожа, темные волосы, серые глаза"
                      className={cl.photo}/>
--- src/components/common/Title/Title.tsx
+++ src/components/common/Title/Title.tsx
@@ -1,7 +1,8 @@
 import React, {FC} from 'react';
 import cl from './title.module.scss'
 
-export const Title:FC<{children: string}> = (props) => {
+
+export const Title:FC<{children: string }> = (props) => {
     return (
         <h1 className={cl.title}>
             {props.children}
